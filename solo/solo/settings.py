3b49b6d2fad61407ffe34c6ab6346f2b91813841
Rashid Jama <rashidjama5@gmail.com>
2020-08-16 18:29:02 -0700

    procfile

--- solo/solo/settings.py
+++ solo/solo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'ideas/media')
 MEDIA_URL = '/media/'
 
 LOGIN_URL = '/ideas/user_login/'
+
+django_heroku.settings(locals())
